ebdc50fdef0abe4eeab6f3c2fb33861407c8a9ee
Valey Austine Senoy <valeyaustinesenoy@gmail.com>
2025-12-09 16:19:26 +0800

    chore: implement user seed and orders seed

--- src/main/resources/db/migration/V3__seed_users.sql
+++ src/main/resources/db/migration/V3__seed_users.sql
@@ -3,4 +3,5 @@ VALUES
     ('John Doe', 'john@example.com', '$2a$10$hCuN0tGxE0BRWvZJr9ScSeC0h5XZ8nHnSHz0F6giptX0R77k8eCOC', NULL, NULL),
     ('Jane Smith', 'jane@example.com', '$2a$10$hCuN0tGxE0BRWvZJr9ScSeC0h5XZ8nHnSHz0F6giptX0R77k8eCOC', NULL, NULL),
     ('Alice Johnson', 'alice@example.com', '$2a$10$hCuN0tGxE0BRWvZJr9ScSeC0h5XZ8nHnSHz0F6giptX0R77k8eCOC', NULL, NULL),
-    ('Bob Williams', 'bob@example.com', '$2a$10$hCuN0tGxE0BRWvZJr9ScSeC0h5XZ8nHnSHz0F6giptX0R77k8eCOC', NULL, NULL);
+    ('Bob Williams', 'bob@example.com', '$2a$10$hCuN0tGxE0BRWvZJr9ScSeC0h5XZ8nHnSHz0F6giptX0R77k8eCOC', NULL, NULL),
+    ('Marie Curry', 'user@example.com', '$2b$12$LSBgAM3pRTHtkid1ITjEG.OhrFmxK9g9N.JK13JiJl7XOY4bywm7O', NULL, NULL);
